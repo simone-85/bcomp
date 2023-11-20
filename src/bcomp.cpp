58267bb21ddc320f935fc76d71961111cce07525
simone-85 <simone.mastroianni.04@gmail.com>
2023-11-20 23:15:36 +0100

    running new functions in the lib

--- src/bcomp.cpp
+++ src/bcomp.cpp
@@ -17,42 +17,26 @@
  */
 #include <iostream>
 #include <cstdint>
-#include <fstream>
-#include <vector>
 #include <iomanip>
-#include "utils/utils.h"
+#include "utils/libbcomputils.h"
 
 const char* progrver = "0.1a";
 
 //ANALYZER PART:
+
 struct byteanalyzed{
 	unsigned char byte_content;
 	int8_t no_occ;
 };
 
-unsigned char* loadbuffer(char* path){
-	std::ifstream in(path, std::ios::binary);
-	if (!in.is_open()) {
-		std::cerr << "Error opening the file." << std::endl;
-		return reinterpret_cast<unsigned char*>(1);
-	}
-	in.seekg(0, std::ios::end);
-	size_t len = in.tellg();
-	in.seekg(0, std::ios::beg);
-	std::cout << "len:" << len << "\n";
-	unsigned char* buffer = new unsigned char[len];
-	in.read(reinterpret_cast<char*>(buffer), len);	
-	return buffer;		
-}
-
 int main(int argc, char *argv[]){
 	version(progrver, &argc);
 	checkparms(&argc, argv);
-
-	/*
-	   for(int i = 0; i < len; i++){
-	   std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(buffer[i]) << " ";
-	   }
-	   std::cout << "\n";
-	*/
+	//until here, all good :-)
+	std::cout << "inpath: " << returnpath(in) << "\n";
+//std::cout << "outpath: " << returnpath(out) << "\n";
+	unsigned char* buffer = loadbuffer(returnpath(in));
+	printBuffer(buffer);
+	countoccurences(buffer);
+	
 }
